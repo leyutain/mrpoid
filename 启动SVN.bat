ba65e785c1cfa054d058b9a329d412da97b36f54
yichouangle <yichouangle@gmail.com>
2012-12-21 01:27:09 +0000

    test

---
+++ 启动SVN.bat
@@ -0,0 +1,3 @@
+sc create svnservice binpath= "H:\Program Files (x86)\WANdisco\Subversion>svnserver.exe --service --root G:\workspace\svn\repository" displayname= "SVNService" depend= tcpip start= auto
+
+pause
